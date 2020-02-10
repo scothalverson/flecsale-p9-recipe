b7c37c92dade0376407e04a40fefe0cfb1679f1b
Scot Halverson <sah@lanl.gov>
2020-02-10 07:39:28 -0800

    using control_replication branch from Legion so that multi-rank runs work correctly

--- 2_download_install_tpl.sh
+++ 2_download_install_tpl.sh
@@ -1,18 +1,22 @@
 module load cuda/10.1.243
 module load gcc/8.3.1
+
+#mpi_imp="mpich@3.3.2"
+#mpi_imp="openmpi@4.0.2 thread_multiple=True fabrics=auto schedulers=auto"
+mpi_imp="spectrum-mpi"
 #gasnet 
 #wget https://gasnet.lbl.gov/download/GASNet-1.32.0.tar.gz
-spack install gasnet@1.32.0%gcc@8.3.1 ^openmpi@4.0.2 thread_multiple=True 
+spack install gasnet@1.32.0%gcc@8.3.1 ^$mpi_imp
 #legion 
 #git clone git@gitlab.com:StanfordLegion/legion.git
 
 #hdf5
-spack install hdf5@1.10.6%gcc@8.3.1 hl=True ^openmpi@4.0.2 thread_multiple=True
+spack install hdf5@1.10.6%gcc@8.3.1 hl=True ^$mpi_imp
 #netcdf
-spack install netcdf-cxx%gcc@8.3.1 ^openmpi@4.0.2 thread_multiple=True
+spack install netcdf-cxx%gcc@8.3.1 ^$mpi_imp
 
 #parmetis, metis
-spack install parmetis%gcc@8.3.1 ^openmpi@4.0.2 thread_multiple=True
+spack install parmetis%gcc@8.3.1 ^$mpi_imp
 
 #spack load -r parmetis - not working as expected on rzansel?
 
@@ -22,6 +26,7 @@ spack install parmetis%gcc@8.3.1 ^openmpi@4.0.2 thread_multiple=True
 `spack module tcl loads | grep "load netcdf-c-"`
 `spack module tcl loads | grep "load netcdf-cxx"`
 `spack module tcl loads | grep "load openmpi-4.0.2-gcc"`
+#`spack module tcl loads | grep "load mpich-3.3.2-gcc"`
 `spack module tcl loads | grep "load gasnet-1.32.0-gcc"`
 
 #if we've just installed spack, rerunning the setup process can help if packages can't be found
@@ -81,17 +86,17 @@ rm -rf legion
 git clone git@gitlab.com:StanfordLegion/legion.git
 cd legion
 git fetch origin
-git checkout kokkos
+#git checkout kokkos
+git checkout control_replication
 mkdir build
 cd build
 
 cmake .. \
 	-DCMAKE_CXX_COMPILER=clang++ \
 	-DCMAKE_C_COMPILER=clang \
+	-DCMAKE_BUILD_TYPE=Debug \
 	-DCMAKE_INSTALL_PREFIX=$curdir/install/ \
 	-DBUILD_SHARED_LIBS=ON \
-	-DLegion_USE_CUDA=ON \
-	-DLegion_USE_Kokkos=ON \
 	-DLegion_USE_GASNet=ON
 
 make -j
