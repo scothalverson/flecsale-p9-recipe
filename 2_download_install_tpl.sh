621ed3356c1027cffaa7515b8e487285ea42f970
Scot Halverson <sah@lanl.gov>
2020-04-17 09:48:24 -0700

    fixes to account for changes in Legion

--- 2_download_install_tpl.sh
+++ 2_download_install_tpl.sh
@@ -1,49 +1,64 @@
 module load cuda/10.1.243
 module load gcc/8.3.1
+
+mpistring=mpich
+#mpistring=openmpi@4.0.2 thread_multiple=True
 #gasnet 
 #wget https://gasnet.lbl.gov/download/GASNet-1.32.0.tar.gz
-spack install gasnet@1.32.0%gcc@8.3.1 ^openmpi@4.0.2 thread_multiple=True 
+spack install gasnet@1.32.0%gcc@8.3.1 ^$mpistring 
 #legion 
 #git clone git@gitlab.com:StanfordLegion/legion.git
 
 #hdf5
-spack install hdf5@1.10.6%gcc@8.3.1 hl=True ^openmpi@4.0.2 thread_multiple=True
+spack install hdf5@1.10.6%gcc@8.3.1 hl=True ^$mpistring
 #netcdf
-spack install netcdf-cxx%gcc@8.3.1 ^openmpi@4.0.2 thread_multiple=True
+spack install netcdf-cxx%gcc@8.3.1 ^$mpistring
 
 #parmetis, metis
-spack install parmetis%gcc@8.3.1 ^openmpi@4.0.2 thread_multiple=True
+spack install parmetis%gcc@8.3.1 ^$mpistring
 
+#exodus via seacas
+spack install seacas cgns=False ^$mpistring 
 #spack load -r parmetis - not working as expected on rzansel?
 
-`spack module tcl loads | grep "load parmetis"`
-`spack module tcl loads | grep "load metis"`
-`spack module tcl loads | grep "load hdf5"`
-`spack module tcl loads | grep "load netcdf-c-"`
-`spack module tcl loads | grep "load netcdf-cxx"`
-`spack module tcl loads | grep "load openmpi-4.0.2-gcc"`
-`spack module tcl loads | grep "load gasnet-1.32.0-gcc"`
+#`spack module tcl loads | grep "load parmetis"`
+#`spack module tcl loads | grep "load metis"`
+#`spack module tcl loads | grep "load hdf5"`
+#`spack module tcl loads | grep "load netcdf-c-"`
+#`spack module tcl loads | grep "load netcdf-cxx"`
+#`spack module tcl loads | grep "load openmpi-4.0.2-gcc"
+#`spack module tcl loads | grep "load mpich"`
+#`spack module tcl loads | grep "load gasnet-1.32.0-gcc"`
+#`spack module tcl loads | grep "seacas"`
+
+
+spack load -r gasnet@1.32.0%gcc@8.3.1 ^$mpistring 
+spack load -r hdf5@1.10.6%gcc@8.3.1 hl=True ^$mpistring
+spack load -r netcdf-cxx%gcc@8.3.1 ^$mpistring
+spack load -r parmetis%gcc@8.3.1 ^$mpistring
+spack load -r seacas cgns=False ^$mpistring 
+
 
 #if we're rerunning this script, we don't want to depend on anything that might already be in the install dir
 rm -rf install
 
-#exodus
-rm -rf exodus-6.09*
 curdir=`pwd`
-wget https://github.com/laristra/flecsi-third-party/raw/master/files/exodus-6.09.tar.gz
-tar -xvf exodus-6.09.tar.gz
-cd exodus-6.09/exodus/
-mkdir build
-cd build
-cmake .. \
-	-DCMAKE_INSTALL_PREFIX:PATH=$curdir/install \
-	-DPYTHON_INSTALL=$curdir/install \
-	-DCMAKE_CXX_COMPILER=clang++ \
-	-DCMAKE_C_COMPILER=clang 
-make 
-make install
+#exodus
+#rm -rf exodus-6.09*
+#wget https://github.com/laristra/flecsi-third-party/raw/master/files/exodus-6.09.tar.gz
+#tar -xvf exodus-6.09.tar.gz
+#cd exodus-6.09/exodus/
+#mkdir build
+#cd build
+#cmake .. \
+#	-DCMAKE_INSTALL_PREFIX:PATH=$curdir/install \
+#	-DPYTHON_INSTALL=$curdir/install \
+#	-DCMAKE_CXX_COMPILER=clang++ \
+#	-DCMAKE_C_COMPILER=clang 
+#make 
+#make install
 
-cd ../../../
+#cd ../../../
 
 #kokkos
 rm -rf kokkos
@@ -76,8 +91,10 @@ echo $CMAKE_PREFIX_PATH
 rm -rf legion
 git clone git@gitlab.com:StanfordLegion/legion.git
 cd legion
-git fetch origin
-git checkout kokkos
+#git fetch origin
+#git checkout kokkos
+git checkout control_replication
+sed -i 's/-march/-mcpu/g' CMakeLists.txt
 mkdir build
 cd build
 
@@ -86,6 +103,7 @@ cmake .. \
 	-DCMAKE_C_COMPILER=clang \
 	-DCMAKE_INSTALL_PREFIX=$curdir/install/ \
 	-DBUILD_SHARED_LIBS=ON \
+	-DCUDA_HOST_COMPILER=`which g++` \
 	-DLegion_USE_CUDA=ON \
 	-DLegion_USE_Kokkos=ON \
 	-DLegion_USE_GASNet=ON
